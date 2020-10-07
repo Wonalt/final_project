2a35cc671c950334d9d1139f0178254ea2bc8071
vitor <784139428@qq.com>
2020-10-07 17:44:37 +0800

    update doc2vec

--- doc2vec.py
+++ doc2vec.py
@@ -32,12 +32,12 @@ def train(x_train, size=200, epoch_num=1):
 
 def test():
     model_dm = Doc2Vec.load("model_doc2vec")
-    text_test = u'在时代下,信息管理与信息系统的应用越发受到人们重视。当下,信息资源炙手可热,尤其是各行各业正在大力发展的情况下,有必要完善信息管理与信息系统建设,创新企业管理模式,实现资源的优化配置和利用。笔者主要对时代下的信息管理与信息系统管理进行了分析。'
+    text_test = u'在时代下,信息管理与信息系统的应用越发受到人们重视饕餮。当下,信息资源炙手可热,尤其是各行各业正在大力发展的情况下,有必要完善信息管理与信息系统建设,创新企业管理模式,实现资源的优化配置和利用。笔者主要对时代下的信息管理与信息系统管理进行了分析。'
     text_raw = jieba.lcut(text_test)
     text_raw = [e for e in text_raw if e not in stop_list]
     inferred_vector_dm = model_dm.infer_vector(text_raw)
     sims = model_dm.docvecs.most_similar([inferred_vector_dm], topn=10)
-
+    #  model_dm.docvecs 为向量化矩阵
     return sims
 
 
